2239928aa8046abf533e2db691ac7c73a2c46297
handyman-sodo <handymansodo@gmail.com>
2024-08-20 16:41:59 +0900

    [feat]상품 등록 수정 삭제 조회 기능

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
-import { ProductsController } from './products.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsService } from './products.service';
+import { ProductsController } from './products.controller';
+import { Product } from './entities/products.entity';
+import { PRODUCT_SERVICE } from './constants/product-service.token';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Product])],
   controllers: [ProductsController],
-  providers: [ProductsService]
+  providers: [
+    {
+      provide: PRODUCT_SERVICE,
+      useClass: ProductsService,
+    },
+  ],
+  exports: [PRODUCT_SERVICE],
 })
+
 export class ProductsModule {}
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from './entities/products.entity';
+import { CreateProductDto, UpdateProductDto } from './dto/products.dto';
+import { IProductService } from './interfaces/products.interface';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService implements IProductService {
+  constructor(
+    @InjectRepository(Product)
+    private productsRepository: Repository<Product>,
+  ) {}
+
+  async create(createProductDto: CreateProductDto): Promise<Product> {
+    const product = this.productsRepository.create(createProductDto);
+    return this.productsRepository.save(product);
+  }
+
+  async findAll(): Promise<Product[]> {
+    return this.productsRepository.find();
+  }
+
+  async findOne(id: number): Promise<Product> {
+    const product = await this.productsRepository.findOne({ where: { id } });
+    if (!product) {
+      throw new NotFoundException(`Product with ID "${id}" not found`);
+    }
+    return product;
+  }
+
+  async update(id: number, updateProductDto: UpdateProductDto): Promise<Product> {
+    const product = await this.findOne(id);
+    Object.assign(product, updateProductDto);
+    return this.productsRepository.save(product);
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.productsRepository.softDelete(id);
+  }
+}
